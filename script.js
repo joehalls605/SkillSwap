9f7db57f75e788dcafeebadc9baab0131382860d
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-20 16:08:21 +0000

    script add

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function submitForm(){
+    const currentSkills = document.getElementById("currentSkills");
+    const desiredSkills = document.getElementById("desiredSkills");
+    const availability = document.getElementById("availability");
+
+    if(!currentSkills || !desiredSkills |!availability){
+        const error = createElement(<p>All fields have to be completed</p>);
+        const notice = document.getElementById("notice");
+        notice.append(error);
+
+    }
+
+
+}
